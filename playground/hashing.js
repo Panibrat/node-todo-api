863da5c9931c7c3a95c1698148f4967df6e8b834
Panibrat <a.panibratenko@gmail.com>
2017-08-01 15:33:45 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,33 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc@Panibrat@4571';
+
+bcrypt.genSalt(12, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(`HASH: ${hash}`);
+    })
+});
+
+var hashedPassword1 = '$2a$10$0OF5mNgGKp91CY12RS.WieliMXgkr.fEqlVHmuM6Xx1ZGAb8GObmq';
+var hashedPassword2 = '$2a$10$WImFi/H6R2fg7PTQa1h8R.g8j6qukv/DWZOnSo8UCbPBubLVUhIJe';
+var hashedPassword3 = '$2a$12$CUe9.zdrn2BND4542oYOmuIAfuEGaVdDLsKLBB7/CjV.mE6KZ3Hci';
+var hashedPassword4 = '$2a$12$VbTYjOPKZOoXvxWs0vH39OZadrpDEIkXTnP7EqOQzFDb9f/6V7/wq';
+var hashedPassword4 = '$2a$12$pKsg4FXgDqRc90rarFzB2eyeE5.qiN/GvxZ0SkOWBaS1os0WQ5ajm';
+
+bcrypt.compare(password, hashedPassword1, (err, res) => {
+    console.log(res);
+});
+bcrypt.compare(password, hashedPassword2, (err, res) => {
+    console.log(res);
+});
+bcrypt.compare(password, hashedPassword3, (err, res) => {
+    console.log(res);
+});
+bcrypt.compare(password, hashedPassword4, (err, res) => {
+    console.log(res);
+});
 
 //let message = 'My name is Sasha';
 let message = 'a';
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const validator = require('validator');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -65,6 +66,22 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function (next) {
+   var user = this;
+   if (user.isModified('password')){
+       var password = user.password;
+       bcrypt.genSalt(10, (err, salt) => {
+           bcrypt.hash(password, salt, (err, hash) => {
+               user.password = hash;
+               //console.log(`HASH: ${hash}`);
+               next();
+           })
+       });
+   } else {
+       next();
+   }
+});
 
 const User = mongoose.model('User', UserSchema);
 
